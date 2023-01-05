e5014303c08dc0da2235c119a176b8a9bf5a9ad5
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-01-05 22:52:34 +0200

    + page and title

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,9 @@
+
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]")
+      openModalBtn: document.querySelector("[data-modil-bnt-open]"),
+      closeModalBtn: document.querySelector("[data-modil-bnt-close]"),
+      modal: document.querySelector("[data-modil-bnt]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
